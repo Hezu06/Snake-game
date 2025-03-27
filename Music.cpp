4554e27dba20727f9fbbf7fc98cdb400acdcfc9a
Hezu <nguyentrunghieu14112006@gmail.com>
2025-03-27 12:38:17 +0700

    update music

--- Music.cpp
+++ Music.cpp
@@ -1,14 +1,10 @@
 #include "Music.h"
 
 Music::Music() {
-	music = Mix_LoadMUS("Music/music.mp3");
+	/*music = Mix_LoadMUS("Music/music.mp3");
 	if (!music) {
 		std::cout << "Failed to load music! SDL_mixer Error: " << Mix_GetError() << std::endl;
-	}
-	moveSound = Mix_LoadWAV("Music/move.mp3");
-	if (!moveSound) {
-		std::cout << "Failed to load move sound! SDL_mixer Error: " << Mix_GetError() << std::endl;
-	}
+	}*/
 	eatSound = Mix_LoadWAV("Music/food.mp3");
 	if (!eatSound) {
 		std::cout << "Failed to load eat sound! SDL_mixer Error: " << Mix_GetError() << std::endl;
@@ -17,4 +13,9 @@ Music::Music() {
 	if (!gameOverSound) {
 		std::cout << "Failed to load game over sound! SDL_mixer Error: " << Mix_GetError() << std::endl;
 	}
+}
+
+Music::~Music() {
+	Mix_FreeChunk(eatSound);
+	Mix_FreeChunk(gameOverSound);
 }
--- Music.h
+++ Music.h
@@ -6,8 +6,6 @@
 struct Music {
 	Music();
 	~Music();
-	Mix_Music* music = nullptr;
-	Mix_Chunk* moveSound = nullptr;
 	Mix_Chunk* eatSound = nullptr;
 	Mix_Chunk* gameOverSound = nullptr;
 };
